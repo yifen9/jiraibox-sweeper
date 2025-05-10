7364f53915d8f41a320bab27dd618c92b137bf3b
Anto <leeantoyf@gmail.com>
2025-05-10 04:04:41 +0200

    update

--- src/main/java/com/yifen9/jiraiboxsweeper/Main.java
+++ src/main/java/com/yifen9/jiraiboxsweeper/Main.java
@@ -4,16 +4,22 @@ import com.yifen9.jiraiboxsweeper.controller.Game;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     public void start(Stage stage) {
-        Game game = new Game();
-        Scene scene = new Scene(game.createContent(), 950, 650);
+        try {
+            Game game = new Game();
+            Scene scene = new Scene(game.createContent(), 950, 650);
 
-        stage.setTitle("Jiraibox Sweeper");
-        stage.setScene(scene);
-        stage.show();
+            stage.setTitle("Jiraibox Sweeper");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, e.toString()).showAndWait();
+        }
     }
 
     public static void main(String[] args) {
